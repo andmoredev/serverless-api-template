1faa2eda771ea8baa544a0d54cde879b8b267bee
Andres Moreno <andrez.moreno.m@gmail.com>
2023-02-21 20:34:21 -0600

    cleanup template a bit

--- {{ cookiecutter.outputDir }}/functions/test-function/index.js
+++ {{ cookiecutter.outputDir }}/functions/test-function/index.js
@@ -1,3 +1,7 @@
-exports.handler = async (event) => {
+const { initializePowertools } = process.env.LAMBDA_TASK_ROOT ?
+  require('/opt/nodejs/lambda-powertools') :
+  require('../../layers/lambda-powertools/lambda-powertools');
+
+exports.handler = initializePowertools(async (event) => {
   return 'hello world';
-};
+});
