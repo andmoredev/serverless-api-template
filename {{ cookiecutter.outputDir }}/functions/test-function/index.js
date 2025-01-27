5eeb9293e8106f6211afc8c3daaf1c64a75cd5c6
Andres Moreno <andres@andmore.dev>
2025-01-26 21:31:14 -0700

    Update to latest version of how I do stuff

--- {{ cookiecutter.outputDir }}/functions/test-function/index.js
+++
@@ -1,7 +0,0 @@
-const { initializePowertools } = process.env.LAMBDA_TASK_ROOT ?
-  require('/opt/nodejs/lambda-powertools-utils') :
-  require('../../layers/lambda-powertools-utils/lambda-powertools-utils');
-
-exports.handler = initializePowertools(async (event) => {
-  return 'hello world';
-});
